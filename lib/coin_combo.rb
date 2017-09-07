b288a29628da551b9dffc676a4c33ff66dba7e8e
Noah Gottlieb, Qudsia Ahmad and Rebecca Mcintosh <qudsia@>
2017-09-07 13:40:53 -0700

    while loop that collects keys.

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -4,7 +4,25 @@ class Coin
     @changes = change
   end
    def coin_maker
-     amount= @changes
-    #  denominations = [25,10,5,1]
+     amount = @changes
+     dens = {'quarter'=> 25, 'dime' => 10, 'nickel' => 5, 'penny' => 1}
+     ch_arrays = []
+     while (amount >= 25)
+       amount -= dens.fetch('quarter')
+       ch_arrays.push('quarter')
+     end
+       while (amount >= 10)
+         amount -= dens.fetch('dime')
+         ch_arrays.push('dime')
+       end
+         while (amount >= 5)
+           amount -= dens.fetch('nickel')
+           ch_arrays.push('nickel')
+         end
+           while (amount >= 1)
+             amount -= dens.fetch('penny')
+             ch_arrays.push('penny')
+            end
+      ch_arrays
   end
 end
